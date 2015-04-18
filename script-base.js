3fa8dede20660b450ca32e7ddf90cb28d6b1db95
Jonathan Pidgeon <jon@empire5design.com>
2015-04-17 21:26:46 -0600

    view generator dependency fix

--- script-base.js
+++ script-base.js
@@ -1,10 +1,11 @@
 'use strict';
 var util = require('util');
 var yeoman = require('yeoman-generator');
+var _s = require('underscore.string');
 
 var Generator = module.exports = function Generator() {
   yeoman.generators.NamedBase.apply(this, arguments);
-  this.classyName = this._.classify(this.name);
+  this.classyName = _s(this.name).classify();
 };
 
 util.inherits(Generator, yeoman.generators.NamedBase);
