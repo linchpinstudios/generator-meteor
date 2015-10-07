f8c41f79356a7608fb970e030696d96ecffc3dd4
Josh Hagel <joshhagel@linchpinstudios.com>
2015-10-07 00:42:17 -0700

    Copy SCSS

--- app/index.js
+++ app/index.js
@@ -99,6 +99,14 @@ Generator.prototype.addBootstrap = function addBootstrap() {
     this.packages.push('twbs:bootstrap');
   }
   this.copy('client/styles/theme.scss', 'client/styles/theme.scss');
+  this.copy('client/styles/partials/_buttons.scss', 'client/styles/partials/_buttons.scss');
+  this.copy('client/styles/partials/_fonts.scss', 'client/styles/partials/_fonts.scss');
+  this.copy('client/styles/partials/_icons.scss', 'client/styles/partials/_icons.scss');
+  this.copy('client/styles/partials/_inputs.scss', 'client/styles/partials/_inputs.scss');
+  this.copy('client/styles/partials/_list.scss', 'client/styles/partials/_list.scss');
+  this.copy('client/styles/partials/_mixins.scss', 'client/styles/partials/_mixins.scss');
+  this.copy('client/styles/partials/_type.scss', 'client/styles/partials/_type.scss');
+  this.copy('client/styles/partials/_visibility.scss' 'client/styles/partials/_visibility.scss');
 };
 
 Generator.prototype.done = function done() {
